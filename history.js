d599ee74f2a7ce44487cdd998128998433904947
ale tandapata <aletandapata@gmail.com>
2017-08-22 17:00:03 -0300

    history json file

---
+++ history.js
@@ -0,0 +1,27 @@
+
+module.exports = {  
+  question1 : {
+    name : 'Pali',
+    msg: "How are you doing?"
+  },
+  
+  question2 : {
+    name : 'Star',
+    msg : 'These is a tender party.',
+    
+  },
+  
+  question3 : {
+    name : 'Seagull',
+    msg : 'Veggies!',
+
+  },
+  
+  question4 : {
+    name : 'Abracadabra',
+    msg : 'Favorite message. ',
+   
+  },
+  
+
+};
